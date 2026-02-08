f133d975d1d3217d154881e0a0af5a0a78ad2297
Wetherish <bartikozlowski@gmail.com>
2026-02-08 20:33:51 +0100

    todo

--- config/config.go
+++ config/config.go
@@ -1,33 +1,53 @@
 package config
 
-type Shortcut struct {
-	Name   string
-	Action func(t any)
+import (
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+type UIConfig struct {
+	BarBackgroundColor       string `toml:"bar_background_color"`
+	BarForegroundColor       string `toml:"bar_foreground_color"`
+	TabBackgroundColor       string `toml:"tab_background_color"`
+	TabForegroundColor       string `toml:"tab_foreground_color"`
+	ActiveTabBackgroundColor string `toml:"active_tab_background_color"`
+	ActiveTabForegroundColor string `toml:"active_tab_foreground_color"`
+	EnableSideBar            bool   `toml:"enable_side_bar"`
 }
 
-func NewShortcut(name string, action func(t any)) Shortcut {
-	return Shortcut{Name: name, Action: action}
+type ApplicationConfig struct {
+}
+
+type KeymapConfig struct {
 }
 
 type Config struct {
-	shortcuts map[string]Shortcut
+	Shortcuts map[string]Shortcut
+	UI        UIConfig          `toml:"ui"`
+	App       ApplicationConfig `toml:"app"`
+	Keymap    KeymapConfig      `toml:"keymap"`
+}
+
+func LoadConfig() *Config {
+	file, err := os.ReadFile("/Users/tymczasowe/Projekt/config.toml")
+	if err != nil {
+		panic(err)
+	}
+
+	var config Config
+	if err := toml.Unmarshal(file, &config); err != nil {
+		panic(err)
+	}
+	config.Shortcuts = make(map[string]Shortcut)
+	return &config
 }
 
 func NewConfig() *Config {
 	return &Config{
-		shortcuts: make(map[string]Shortcut),
+		Shortcuts: make(map[string]Shortcut),
+		UI:        UIConfig{},
+		App:       ApplicationConfig{},
+		Keymap:    KeymapConfig{},
 	}
-}
-
-func (c *Config) RegisterShortcut(key string, shortcut Shortcut) {
-	c.shortcuts[key] = shortcut
-}
-
-func (c *Config) FindAndInvoke(key string, t any) bool {
-	value, ok := c.shortcuts[key]
-	if ok {
-		value.Action(t)
-		return true
-	}
-	return false
 }
--- session/manager.go
+++ session/manager.go
@@ -10,19 +10,17 @@ import (
 	"sync"
 	"syscall"
 
-	"github.com/charmbracelet/lipgloss"
 	"github.com/creack/pty"
 	"golang.org/x/term"
 )
 
 type Manager struct {
-	Sessions     []*Session
-	ActiveIdx    int
-	Mu           sync.Mutex
-	Config       *config.Config
-	nextID       int
-	winSize      *pty.Winsize
-	scrollRegion string
+	Sessions  []*Session
+	ActiveIdx int
+	Mu        sync.Mutex
+	Config    *config.Config
+	nextID    int
+	winSize   *pty.Winsize
 }
 
 func MakeAction[T any](f func(T)) func(any) {
@@ -37,7 +35,7 @@ func NewManager() *Manager {
 	mgr := &Manager{
 		Sessions:  make([]*Session, 0),
 		ActiveIdx: 0,
-		Config:    config.NewConfig(),
+		Config:    config.LoadConfig(),
 		nextID:    1,
 	}
 
@@ -47,10 +45,16 @@ func NewManager() *Manager {
 
 	newTab := MakeAction(func(m *Manager) {
 		m.AddSession()
-		m.DrawFooter()
+		m.Draw()
 	})
 
+	// loadNewConfig := MakeAction(func(m *Manager) {
+	// 	m.Config = config.LoadConfig()
+	// 	m.Draw()
+	// })
+
 	mgr.Config.RegisterShortcut(config.CtrlN, config.NewShortcut("New Tab", newTab))
+	// mgr.Config.RegisterShortcut(config.CtrlB, config.NewShortcut("Load New Config", loadNewConfig))
 	mgr.Config.RegisterShortcut(config.ShiftTab, config.NewShortcut("Switch Tab", switchTab))
 
 	return mgr
@@ -110,7 +114,7 @@ func (m *Manager) streamSessionOutput(s *Session) {
 			os.Stdout.Write(data)
 			if hasClear {
 				m.Mu.Unlock()
-				m.DrawFooter()
+				m.Draw()
 			} else {
 				m.Mu.Unlock()
 			}
@@ -146,7 +150,7 @@ func (m *Manager) SwitchTab() {
 	}
 
 	m.Mu.Unlock()
-	m.DrawFooter()
+	m.Draw()
 	m.Mu.Lock()
 }
 
@@ -173,60 +177,29 @@ func (m *Manager) RemoveSession(id int) {
 			sNew.Mu.Unlock()
 
 			m.Mu.Unlock()
-			m.DrawFooter()
+			m.Draw()
 			m.Mu.Lock()
 			return
 		}
 	}
 }
 
-func (m *Manager) DrawFooter() {
+func (m *Manager) Draw() {
 	width, height, err := term.GetSize(int(os.Stdin.Fd()))
 	if err != nil {
 		return
 	}
 
-	// Styles
-	styleBar := lipgloss.NewStyle().
-		Background(lipgloss.Color("#B4C0DA")).
-		Foreground(lipgloss.Color("#484D57")).
-		Padding(0, 1)
-
-	styleTab := lipgloss.NewStyle().
-		Background(lipgloss.Color("#DACEB4")).
-		Foreground(lipgloss.Color("#484D57")).
-		Padding(0, 1)
-
-	styleActiveTab := lipgloss.NewStyle().
-		Background(lipgloss.Color("#B4DABB")).
-		Foreground(lipgloss.Color("#484D57")).
-		Padding(0, 1)
-
 	m.Mu.Lock()
-	displayIdx := m.ActiveIdx + 1
-	totalSessions := len(m.Sessions)
+	sessions := m.Sessions
+	activeIdx := m.ActiveIdx
+	conf := m.Config.UI
 	m.Mu.Unlock()
-
-	// Build Tab List
-	var tabs []string
-	for i := 0; i < totalSessions; i++ {
-		t := fmt.Sprintf("Tab %d", i+1)
-		if i == m.ActiveIdx {
-			tabs = append(tabs, styleActiveTab.Render(t))
-		} else {
-			tabs = append(tabs, styleTab.Render(t))
-		}
-	}
-
-	// Status Bar
-	statusText := fmt.Sprintf("TaskFlow | %d/%d | Ctrl+N: New | Ctrl+B: Switch | Ctrl+Q: Quit", displayIdx, totalSessions)
-	status := styleBar.Width(width - lipgloss.Width(lipgloss.JoinHorizontal(lipgloss.Top, tabs...))).Render(statusText)
-
-	footer := lipgloss.JoinHorizontal(lipgloss.Top, append(tabs, status)...)
-
-	// Enforce width
-	if lipgloss.Width(footer) < width {
-		footer = lipgloss.NewStyle().Width(width).Background(lipgloss.Color("62")).Render(footer) // fill remaining
+	var bar string
+	if conf.EnableSideBar {
+		bar = RenderSideBar(conf, sessions, activeIdx, width)
+	} else {
+		bar = RenderFooter(conf, sessions, activeIdx, width)
 	}
 
 	fmt.Print("\0337")
@@ -234,16 +207,17 @@ func (m *Manager) DrawFooter() {
 	fmt.Print(region)
 
 	m.Mu.Lock()
-	m.scrollRegion = region
 	m.winSize = &pty.Winsize{Rows: uint16(height - 1), Cols: uint16(width)}
 	for _, s := range m.Sessions {
 		pty.Setsize(s.Ptmx, m.winSize)
 	}
 	m.Mu.Unlock()
 
-	// Move to bottom and print
-	fmt.Printf("\033[%d;1f\033[2K%s", height, footer)
-
+	if conf.EnableSideBar {
+		fmt.Printf("\033[1;1f\033[2K%s", bar)
+	} else {
+		fmt.Printf("\033[%d;1f\033[2K%s", height, bar)
+	}
 	fmt.Print("\0338")
 }
 
@@ -255,7 +229,7 @@ func (m *Manager) Run() {
 	signal.Notify(ch, syscall.SIGWINCH)
 	go func() {
 		for range ch {
-			m.DrawFooter()
+			m.Draw()
 		}
 	}()
 	ch <- syscall.SIGWINCH
